32a232739aeed79ec444fa230a3000406dd011a2
Christian Heimes <christian@python.org>
2021-11-16 12:33:19 +0100

    Fail when extensions are missing

--- tests/entry.sh
+++ tests/entry.sh
@@ -8,6 +8,16 @@ if ! test -e /cpython/pyconfig.h.in; then
     exit 1
 fi
 
+case "$PYBUILDDEP_DISTRO" in
+    centos:7|rhel:7)
+        # OpenSSL 1.1.1 missing
+        ;;
+    *)
+        # fail on missing stdlib extension module
+        export PYTHONSTRICTEXTENSIONBUILD=1
+        ;;
+esac
+
 cd /cpython
 ./configure -C
 make -j4
