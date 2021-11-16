72b900d11a2114456885033f795c2fe3722f7571
Christian Heimes <christian@python.org>
2021-11-16 10:44:27 +0100

    Use command instead of entrypoint

--- tests/entry.sh
+++ tests/entry.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -ex
 
+SENTINEL=${SRC}/pyconfig.h.in
+
+if ! test -e /cpython/pyconfig.h.in; then
+    echo "/cpython/pyconfig.h.in missing" >&2
+    exit 1
+fi
+
 cd /cpython
 ./configure -C
 make -j4
